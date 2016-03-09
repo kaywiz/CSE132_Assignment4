2c831a968e2da2546a7aedda074e6e056ee4553e
k.wisneski@ACCOUNTS.AD.WUSTL.EDU <k.wisneski@ACCOUNTS.AD.WUSTL.EDU@5079ff04-d25d-4c85-8423-372005a2952e>
2016-03-09 19:14:56 +0000

    almost complete 5, nearly done 4

--- cse132_sp16/java/assignment4/ViewInputStream.java
+++ cse132_sp16/java/assignment4/ViewInputStream.java
@@ -2,39 +2,35 @@ package assignment4;
 
 import java.io.*;
 
-
 import javax.swing.JFrame;
 
 import studio4.PrintStreamPanel;
 
 public class ViewInputStream extends FilterInputStream {
 
-    final private PrintStreamPanel psp;
-    final private PrintStream ps;
+	final private PrintStreamPanel psp;
+	final private PrintStream ps;
 
-    public ViewInputStream(InputStream in) {
-        super(in);
-        JFrame f = new JFrame("ViewInputStream");
-		f.setBounds(100,100,225,300);
-        psp = new PrintStreamPanel();
+	public enum State {
+		waitingForMagicNumber, waitingForInputType, readingDebugString, readingErrorString, readingTimestamp, readingPoten, readingRawTemp, readingConvertTemp, readingFilteredTemp
+	}
+
+	public ViewInputStream(InputStream in) {
+		super(in);
+		JFrame f = new JFrame("ViewInputStream");
+		f.setBounds(100, 100, 225, 300);
+		psp = new PrintStreamPanel();
 		f.getContentPane().add(psp);
 		f.setVisible(true);
-        ps = psp.getPrintStream(); 
-    }
+		ps = psp.getPrintStream();
+	}
 
-    public int read() {
-        // TODO
-        // use super.read() to access the next byte from the InputStream
-        // also, you can use ps like System.out to print to the new window
-    	try {
-			int x = super.read();
-			String hex = Integer.toHexString(x);
-			PrintStreamPanel.PrintStream(in);
-			return x;
-		} catch (IOException e) {
-			// FIXME Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-    
+	public int read() throws IOException {
+		int x = super.read();
+		String hex = Integer.toHexString(x);
+		ps.println(hex);
+		return x;
+
+	}
+
 }
